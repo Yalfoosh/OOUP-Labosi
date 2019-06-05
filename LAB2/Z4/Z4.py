d88ee6d570b68ef35a482ff5fe7c29f683fd7156
Ljac <ms50144@fer.hr>
2019-06-05 13:29:59 +0200

    no message

--- LAB2/Z4/Z4.py
+++ LAB2/Z4/Z4.py
@@ -3,10 +3,10 @@ from globals import Generator, DistributionTester,\
 
 
 def test():
-    tester_nearest_rank = DistributionTester(generation_mode=Generator.Interval,
+    tester_nearest_rank = DistributionTester(generation_function=Generator.generate_interval,
                                              percentile_function=nearest_rank,
                                              start=5, stop=100, step=10)
-    tester_lerp = DistributionTester(generation_mode=Generator.Gaussian,
+    tester_lerp = DistributionTester(generation_function=Generator.generate_gaussian,
                                      percentile_function=closest_rank_lerp,
                                      mean=50, variance=50, count=10)
 
--- LAB2/Z4/globals.py
+++ LAB2/Z4/globals.py
@@ -1,52 +1,44 @@
 import numpy as np
 
 from copy import deepcopy
-from enum import Enum, auto
 from math import ceil
 from typing import List, Callable
 
 
 # region Number generation
-class Generator(Enum):
-    Interval = auto(),
-    Gaussian = auto(),
-    Fibonacci = auto()
+class Generator:
+    @staticmethod
+    def __fibonacci_sequence(count: int = 1) -> List[int]:
+        assert count > 0
 
+        fibonacci_numbers: List[int] = list()
 
-def fibonacci_sequence(count: int = 1) -> List[int]:
-    assert count > 0
+        for i in range(0, count):
+            if i < 2:
+                fibonacci_numbers.append(1)
+            else:
+                fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
 
-    fibonacci_numbers: List[int] = list()
+        return fibonacci_numbers
 
-    for i in range(0, count):
-        if i < 2:
-            fibonacci_numbers.append(1)
-        else:
-            fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
+    @staticmethod
+    def generate_interval(**kwargs):
+        return list(range(kwargs.get("start", 0), kwargs.get("stop", 10), kwargs.get("step", 1)))
 
-    return fibonacci_numbers
-
-
-def generate_random_integers(mode: Generator, **kwargs) -> List[int]:
-    if mode is Generator.Interval:
-        return list(range(kwargs.get("start", 0),
-                          kwargs.get("stop", 10),
-                          kwargs.get("step", 1)))
-    elif mode is Generator.Gaussian:
+    @staticmethod
+    def generate_gaussian(**kwargs):
         return list(map(lambda x: int(x),
                         np.random.normal(kwargs.get("mean", 0),
                                          kwargs.get("variance", 1),
                                          kwargs.get("count", 10))))
-    elif mode is Generator.Fibonacci:
-        return fibonacci_sequence(kwargs.get("count", 10))
-    else:
-        raise NotImplementedError
+
+    @staticmethod
+    def generate_fibonacci(**kwargs):
+        return Generator.__fibonacci_sequence(kwargs.get("count", 10))
 # endregion
 
 
 # region Percentiles
-
-
 def nearest_rank(numbers: List[int], n: int = 80) -> int:
     assert 0 < n < 101
 
@@ -85,14 +77,14 @@ def closest_rank_lerp(numbers: List[int], n: int = 80) -> int:
 
 class DistributionTester:
     def __init__(self,
-                 generation_mode: Generator = None,
+                 generation_function: Callable = None,
                  percentile_function: Callable = None,
                  **kwargs):
-        if generation_mode is None:
-            generation_mode = Generator.Interval
+        if generation_function is None:
             kwargs.update({"start": 0, "stop": 100, "step": 2})
+            generation_function = Generator.generate_interval
 
-        self._numbers = generate_random_integers(generation_mode, **kwargs)
+        self._numbers = generation_function(**kwargs)
         self._numbers.sort()
         self.percentile_function = percentile_function
 
@@ -107,5 +99,4 @@ class DistributionTester:
                 percentile_function = self.percentile_function
 
         return percentile_function(self._numbers, n)
-
 # endregion
